a693ec4d5b19a9f0343f0e033c9b282e2d1077b2
misrar09 <misrar09@gmail.com>
2023-11-20 16:13:49 +0100

    10 email generated on page

--- main.js
+++ main.js
@@ -3,29 +3,28 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-        emails: [],
-
+            emailList: [],
 
         }
     },
     methods: {
 
-        randomMails(){
-            
+        randomMails() {
+
+            this.emailList = []; // Clear the existing emailList
             for (let i = 0; i < 10; i++) {
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(response => {
-                let respEmail = response.data.response;
-                this.emails.push(respEmail);
-            });
-        
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(response => {
+                    let respEmail = response.data.response;
+                    this.emailList.push(respEmail);
+                });
+
+            }
+
         }
-        
-        }
-        
+
     },
     mounted() {
-        
 
-     
+
     }
 }).mount('#app')
