645c2508897871bb932dda260b4ce6b98c29f0f0
misrar09 <misrar09@gmail.com>
2023-11-20 17:37:13 +0100

    Bonus Done using counter

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ createApp({
     data() {
         return {
             emailList: [],
-
+            listDone: false,
         }
     },
     methods: {
@@ -12,10 +12,20 @@ createApp({
         randomMails() {
 
             this.emailList = []; // Clear the existing emailList
+            this.listDone = false;
+            let counter = 0; //Using a counter to count the number of items got
+
             for (let i = 0; i < 10; i++) {
                 axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(response => {
                     let respEmail = response.data.response;
                     this.emailList.push(respEmail);
+
+                    counter++ //Increment the counter
+
+                    if(counter === 10){
+                        this.listDone = true;
+                    }
+
                 });
 
             }
@@ -24,7 +34,7 @@ createApp({
 
     },
     mounted() {
+        
 
-
-    }
+    },
 }).mount('#app')
